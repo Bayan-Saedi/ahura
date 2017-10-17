1fc1270eb43da1aadce542b0176c6d31123f767b
speide <32034144+speide@users.noreply.github.com>
2017-10-17 18:44:31 +0330

    Update rewrite-exercise.php

--- rewrite-exercise.php
+++ rewrite-exercise.php
@@ -1,16 +1,25 @@
 <?php
-function string($str)
-{
-	// convert the first letter of the text to uppercase
-	$str = ucfirst($str);
 
-	// convert the none proper nouns to lowercase
+ function string($str)
+ {
+    // convert the first letter of the text to uppercase
+    $str = ucfirst($str);
+
+    // convert the none proper nouns to lowercase
     $str = preg_replace_callback('/\b([a-z]+\S+)\b/',
-        create_function('$matches', 'return strtolower($matches[0]);'), $str);
+        function($matches) {
+        	return strtolower($matches[0]);
+        }, $str);
 
     // convert the first letter of the sentence to uppercase
     $str = preg_replace_callback('/[.!?] .*?\w/',
-        create_function('$matches', 'return strtoupper($matches[0]);'), $str);
+        function($matches) {
+        	return strtoupper($matches[0]);
+        }, $str);
 
-    var_dump($str);
+    echo($str);
 }
+
+$str = 'hi, weLCome to Ahura companY. good luck.';
+
+string($str);
